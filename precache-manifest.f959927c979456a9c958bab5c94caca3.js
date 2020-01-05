f70cdbd7e0018c543dc44c7124bec71d131c1c59
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-01-05 15:58:12 +1100

    Updates

---
+++ precache-manifest.f959927c979456a9c958bab5c94caca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b62cd0202a1e106854a377ca41a224b",
+    "url": "/project-meal-picker/index.html"
+  },
+  {
+    "revision": "903a8db7a1850ae07ef3",
+    "url": "/project-meal-picker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "190e40be48cf39ef6dee",
+    "url": "/project-meal-picker/static/js/2.f44a90af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project-meal-picker/static/js/2.f44a90af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "903a8db7a1850ae07ef3",
+    "url": "/project-meal-picker/static/js/main.077f023b.chunk.js"
+  },
+  {
+    "revision": "b5931232a993120cf360",
+    "url": "/project-meal-picker/static/js/runtime-main.c2800448.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-meal-picker/static/media/logo.5d5d9eef.svg"
+  }
+]);
